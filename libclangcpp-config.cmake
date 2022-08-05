41b0208e66f1e5d3aec47773617a99d0ee3cfd7d
Fabian Jung <git@fabian-jung.net>
2022-08-05 12:07:00 +0200

    Fix build pipeline

--- libclangcpp-config.cmake
+++ libclangcpp-config.cmake
@@ -1,17 +1,17 @@
-find_library(libclangcpp "libclang-cpp.so" PATHS "/usr/lib/")
+find_library(libclangcpp "libclang-cpp.so" PATHS "/usr/lib/" "/usr/lib/llvm-14/lib")
 message(STATUS ${libclangcpp})
 if(NOT libclangcpp)
     message(FATAL_ERROR "libclang-cpp could not be found. Make sure it is installed on your sytem")
 endif()
 
-find_library(libllvm "libLLVM.so" PATHS "/usr/lib/")
+find_library(libllvm "libLLVM.so" PATHS "/usr/lib/" "/usr/lib/llvm-14/lib")
 if(NOT libllvm)
     message(FATAL_ERROR "libLLVM could not be found. Make sure it is installed on your sytem")
 endif()
 
 add_library(LibClangCpp INTERFACE)
 add_library(LibClangCpp::LibClangCpp ALIAS LibClangCpp)
-# target_include_directories(LibClangCpp INTERFACE /usr/lib/llvm-14/include)
+target_include_directories(LibClangCpp INTERFACE /usr/lib/llvm-14/include)
 
 target_link_libraries(LibClangCpp INTERFACE ${libllvm} ${libclangcpp})
 target_compile_features(LibClangCpp INTERFACE cxx_std_17)
