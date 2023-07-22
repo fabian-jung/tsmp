281f2a6faff9a414f6813603b692c893feb0df47
Fabian Jung <git@fabian-jung.net>
2023-07-22 09:30:04 +0200

    Remove unused functions

--- tooling/engine/utils.h
+++ tooling/engine/utils.h
@@ -12,16 +12,5 @@
 
 namespace utils
 {
-    std::vector<std::string> getSyntaxOnlyToolArgs(const std::vector<std::string> &ExtraArgs, llvm::StringRef FileName);
-
-    bool customRunToolOnCodeWithArgs(std::unique_ptr<clang::FrontendAction> frontendAction, const llvm::Twine &Code,
-                                     const std::vector<std::string> &Args, const llvm::Twine &FileName,
-                                     const clang::tooling::FileContentMappings &VirtualMappedFiles = clang::tooling::FileContentMappings());
-
-
-    bool fileExists(const std::string &file);
-    std::vector<std::string> getCompileArgs(const clang::tooling::CompileCommand& compileCommands);
-    std::string getSourceCode(const std::string &sourceFile);
-
     std::vector<std::string> getClangBuiltInIncludePath(const std::string &fullCallPath);
 }
