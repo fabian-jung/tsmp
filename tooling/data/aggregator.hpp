f175ed6793c5a8a9c5acb082eff73a6787c0a0ff
Fabian Jung <git@fabian-jung.net>
2023-07-29 20:34:41 +0200

    bugfix: the aggregator_t could leak memory if insertion  failed

--- tooling/data/aggregator.hpp
+++ tooling/data/aggregator.hpp
@@ -8,7 +8,7 @@ namespace data {
 
 template <class T>
 struct name_compare_t {
-    bool operator()(const T* lhs, const T* rhs) const {
+    bool operator()(const std::unique_ptr<const T>& lhs, const std::unique_ptr<const T>& rhs) const {
         return lhs->get_name() < rhs->get_name();
     }
 };
@@ -16,25 +16,16 @@ struct name_compare_t {
 class reflection_aggregator_t {
 public:
 
-    reflection_aggregator_t() {
-        free(m_entries);
-    }
-
     template <class T>
-    decltype(auto) allocate() const {
-        return reinterpret_cast<T*>(::operator new(sizeof(T)));
-    }
-
-    template <class T>
-    decltype(auto) emplace(const T* element) {
-        auto [it, success] = fetch<T>().emplace(element);
-        return *it;
+    decltype(auto) emplace(std::unique_ptr<const T> element) {
+        auto [it, success] = fetch<T>().emplace(std::move(element));
+        return it->get();
     }
 
     template <class T, class... Args>
     decltype(auto) create(Args&&... args) {
-        auto [it, success] = fetch<T>().emplace(new T(args...));
-        return *it;
+        auto [it, success] = fetch<T>().emplace(std::make_unique<T>(std::forward<Args>(args)...));
+        return it->get();
     }
     
     template <class T>
@@ -43,7 +34,10 @@ public:
     }
 
     template <class T>
-    using entry_container_t = std::set<const T*, name_compare_t<T>>;
+    using entry_pointer_t = std::unique_ptr<const T>;
+
+    template <class T>
+    using entry_container_t = std::set<entry_pointer_t<T>, name_compare_t<T>>;
 
 private:
     template <class T>
@@ -53,16 +47,6 @@ private:
 
     template <class... T>
     using entries_t = std::tuple<entry_container_t<T>...>;
-
-    template <class T>
-    void free(entry_container_t<T>& container) {
-        std::for_each(container.begin(), container.end(), [](auto* ptr) { delete ptr; });
-    }
-
-    template <class... T>
-    void free(std::tuple<entry_container_t<T>...>) {
-        (free(fetch<T>()), ...);
-    }
 
     entries_t<record_t, enum_t, builtin_t, reference_t, pointer_t, cv_qualified_type_t, constant_array_t> m_entries; 
 };
